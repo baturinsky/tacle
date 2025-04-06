ba376d5ffb7041b14c0a9b8830f0b63284643fc8
baturinsky <baturinsky@gmail.com>
2025-04-06 23:07:39 +0500

    tiles

---
+++ src/a.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="a" tilewidth="16" tileheight="16" spacing="2" margin="2" tilecount="196" columns="14">
+ <image source="../public/tiles.png" width="256" height="256"/>
+</tileset>
